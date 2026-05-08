26403cd702f5ca7991222176e0298882ccc14953
Rivindi Udugama <rudugama@our.ecu.edu.au>
2026-05-08 20:01:05 +0530

    Add species check and kitty DNA adjustment

--- Lesson_02_second_part.sol
+++ Lesson_02_second_part.sol
@@ -27,6 +27,9 @@ contract ZombieFeeding is ZombieFactory {
         Zombie storage myZombie = zombies[_zombieId];
         _targetDna = _targetDna % dnaModulus;
         uint newDna = (myZombie.dna + _targetDna) / 2;
+        if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
+        newDna = newDna - newDna % 100 + 99;
+    }
         _createZombie("NoName", newDna);
     }
 
