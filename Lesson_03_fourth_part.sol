c1bf4dc64fe44c057acc2ec3a7e11122608f2ce1
Rivindi Udugama <rudugama@our.ecu.edu.au>
2026-05-10 14:43:53 +0530

    Add array of values for all the zombies in getZombiesByOwner function

--- Lesson_03_fourth_part.sol
+++ Lesson_03_fourth_part.sol
@@ -20,5 +20,7 @@ contract ZombieHelper is ZombieFeeding {
     }
 
     function getZombiesByOwner(address _owner) external view returns(uint[] memory) {
+        uint[] memory result = new uint[] (ownerZombieCount[_owner]);
+        return result;
     }
 }
