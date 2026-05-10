aa125d764631bdf4bec40ed6f0011a8dd668f1dd
Rivindi Udugama <rudugama@our.ecu.edu.au>
2026-05-10 12:35:16 +0530

    Add  _isReady function

--- Lesson_03_second_part.sol
+++ Lesson_03_second_part.sol
@@ -28,7 +28,10 @@ contract ZombieFeeding is ZombieFactory {
         _zombie.readyTime = uint32(now + cooldownTime);
     }
 
-    
+    function _isReady (Zombie storage _zombie) internal view returns (bool){
+        return (_zombie.readyTime <= now);
+    }
+
 
     function feedAndMultiply (uint _zombieId, uint _targetDna, string memory _species) public{
         require(msg.sender==zombieToOwner[_zombieId]);
