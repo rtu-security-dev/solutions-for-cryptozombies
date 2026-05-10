4c0e709fd041221b1c649b618acc042bd5c7096e
Rivindi Udugama <rudugama@our.ecu.edu.au>
2026-05-10 14:10:36 +0530

    Add aboveLevel modifier to restrict level access

--- Lesson_03_fourth_part.sol
+++ Lesson_03_fourth_part.sol
@@ -4,6 +4,9 @@ import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
 
-  // Start here
+    modifier aboveLevel( uint _level, uint _zombieId){
+        require(zombies[_zombieId].level >= _level);
+        _;
+    }
 
 }
