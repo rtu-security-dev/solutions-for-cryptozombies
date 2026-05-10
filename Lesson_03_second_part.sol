8f8ce108dda85d4728fe1071b5dcf0574dac32da
Rivindi Udugama <rudugama@our.ecu.edu.au>
2026-05-10 10:48:05 +0530

    Add kittyContract as a declaration of KittyInterface

--- Lesson_03_second_part.sol
+++ Lesson_03_second_part.sol
@@ -19,8 +19,7 @@ contrcat KittyInterface {
 
 contract ZombieFeeding is ZombieFactory {
 
-    address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
-    KittyInterface kittyContract = KittyInterface(ckAddress);
+    KittyInterface kittyContract;
 
     function feedAndMultiply (uint _zombieId, uint _targetDna, string memory _species) public{
         require(msg.sender==zombieToOwner[_zombieId]);
