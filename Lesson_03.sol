ef9ae7ec423afe9fae434e5f6cb549db964f3114
Rivindi Udugama <rudugama@our.ecu.edu.au>
2026-05-10 12:08:16 +0530

    Add level and readytime to Zombie str

--- Lesson_03.sol
+++ Lesson_03.sol
@@ -24,7 +24,7 @@ contract ZombieFactory is Ownable{
     mapping (address => uint) ownerZombieCount;
 
     function _createZombie (string memory _name, uint _dna) internal {
-        uint id = zombies.push(Zombie(_name, _dna)) -1;
+        uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime))) -1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
