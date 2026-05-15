17db39e53f0a2813bb9a068cf0b3c01b51fd86e2
Rivindi Udugama <rudugama@our.ecu.edu.au>
2026-05-15 16:19:54 +0530

    Add storage pointer to Zombie in attack ()

--- zombieattack.sol
+++ zombieattack.sol
@@ -12,6 +12,7 @@ contract ZombieAttack is ZombieHelper {
     }
 
     function attack (uint _zombieId, uint _targetId) external ownerOf(_zombieId){
-    
+        Zombie storage myZombie = zombies[_zombieId];
+        Zombie storage enemyZombie = zombies[_targetId];
     }
 }
