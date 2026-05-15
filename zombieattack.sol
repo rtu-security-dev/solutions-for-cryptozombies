72f959dc59cc5403d3c7d658eea9234d955b9f66
Rivindi Udugama <rudugama@our.ecu.edu.au>
2026-05-15 16:17:09 +0530

    Add ownerOf modifer to attack ()

--- zombieattack.sol
+++ zombieattack.sol
@@ -11,7 +11,7 @@ contract ZombieAttack is ZombieHelper {
         return uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % _modulus;
     }
 
-    function attack (uint _zombieId, uint _targetId) external{
+    function attack (uint _zombieId, uint _targetId) external ownerOf(_zombieId){
     
     }
 }
