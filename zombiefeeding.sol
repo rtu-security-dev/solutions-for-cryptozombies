f7b2029996ae71abe32c3b4cb223ef3734cbf591
Rivindi Udugama <rudugama@our.ecu.edu.au>
2026-05-15 16:08:53 +0530

    Fix argument name in ownerOf modifier

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -39,7 +39,7 @@ contract ZombieFeeding is ZombieFactory {
     }
 
 
-    function feedAndMultiply (uint _zombieId, uint _targetDna, string memory _species) internal ownerOf(_zombieID){
+    function feedAndMultiply (uint _zombieId, uint _targetDna, string memory _species) internal ownerOf(_zombieId){
         require(msg.sender==zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
         require(_isReady(myZombie));
