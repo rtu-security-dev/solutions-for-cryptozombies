860bd0eeb1f7d5d97caa083dea1bffdf327dc0f9
Rivindi Udugama <rudugama@our.ecu.edu.au>
2026-05-15 13:18:30 +0530

    Fix spacing in ownerOf modifier

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -21,9 +21,9 @@ contract ZombieFeeding is ZombieFactory {
 
     KittyInterface kittyContract;
 
-    modifier ownerOf(uint _zombieId){
-    require(msg.sender == zombieToOwner[_zombieId]);
-    _;
+    modifier ownerOf(uint _zombieId) {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        _;
     }
 
     function setKittyContractAddress (address _address) external onlyOwner{
