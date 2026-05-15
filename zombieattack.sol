e913d175b8eeebc9c3d81ec132a306eecae7be7e
Rivindi Udugama <rudugama@our.ecu.edu.au>
2026-05-15 16:23:16 +0530

    Add random number to attack () using the randMod()

--- zombieattack.sol
+++ zombieattack.sol
@@ -14,5 +14,6 @@ contract ZombieAttack is ZombieHelper {
     function attack (uint _zombieId, uint _targetId) external ownerOf(_zombieId){
         Zombie storage myZombie = zombies[_zombieId];
         Zombie storage enemyZombie = zombies[_targetId];
+        uint rand = randMod(100);
     }
 }
